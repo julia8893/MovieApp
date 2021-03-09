68103062e9f17a8c1aa23b15b60133ba19299b3b
c1910475048 <julia.frings@stud.fh-campuswien.ac.at>
2021-03-09 11:46:11 +0100

    Third Commit

--- app/src/main/java/at/ac/fhcampuswien/movieapp/ui/MovieViewModel.kt
+++ app/src/main/java/at/ac/fhcampuswien/movieapp/ui/MovieViewModel.kt
@@ -3,23 +3,31 @@ package at.ac.fhcampuswien.movieapp.ui
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import at.ac.fhcampuswien.movieapp.data.Movie
 import at.ac.fhcampuswien.movieapp.data.MovieGenerator
 
-/*
-@BindingMethods(
-    BindingMethod(
-        type = TextView::class,
-        attribute = "android:text",
-        method = "setText"
-    )
-)
+// CUSTOM
+// Properly render the list data in your layout.xml file by using BindingAdapters
+@BindingAdapter("android:customJulia")
+fun setCustomJulia(view: TextView, value: Any?) {
+    value?.let {
+        when (value) {
+            is List<*> -> {
+                val separator = ", "
+                val newString = value.joinToString(separator)
+                view.text = newString
+            }
+            is String -> view.text = value.toString()
+        }
+    }
+}
 
- */
-
+// OVERWRITE
 // Properly render the list data in your layout.xml file by using BindingAdapters
 @BindingAdapter("android:text")
 fun setText(view: TextView, value: Any?) {
@@ -35,6 +43,7 @@ fun setText(view: TextView, value: Any?) {
     }
 }
 
+// OVERWRITE
 // Add a setter to the rating attribute, that only sets the rating value if it is between 0 and 5
 @BindingAdapter("android:rating")
 fun rating(view: RatingBar, value: Float) {
